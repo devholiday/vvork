0c8de92d69ccae18157d5fd33aa4a3371f5c05c2
Albert <mashiahov@gmail.com>
2022-08-29 14:08:21 +0300

    set type number to salary and break in worker shifts

--- src/routes/work-worker-edit.jsx
+++ src/routes/work-worker-edit.jsx
@@ -6,6 +6,14 @@ import Schedule from "../components/schedule/Schedule";
 import {useNavigate, useParams} from "react-router-dom";
 import style from "../components/event-new/EventNew.module.css";
 import ShiftsWorkerAdd from "../components/shift-worker-add/ShiftsWorkerAdd";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object({
+    title: yup.string().max(255, 'Название должно быть не более 255 символов').required('Название обязательное поле'),
+    salary: yup.number().typeError('Зарплата должна быть числом').max(99, 'Зарплата должна быть не более 2 символов').positive('Зарплата должна быть положительным числом').integer().required('Зарплата обязательное поле'),
+    break: yup.number().typeError('Перерыв должен быть числом').max(99, 'Перерыв должен быть не более 3 символов').moreThan(-1, 'Перерыв должен быть положительным числом').integer(),
+}).required();
 
 export default function WorkWorkerEdit () {
     const [event, setEvent] = useState();
@@ -15,7 +23,9 @@ export default function WorkWorkerEdit () {
     const db = getFirestore();
     const auth = getAuth();
 
-    const { register, handleSubmit, reset, getValues } = useForm();
+    const { register, handleSubmit, reset, getValues, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -61,14 +71,18 @@ export default function WorkWorkerEdit () {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-block">
                     <input {...register("title")} placeholder="Название" />
+                    <p className="errorInput">{errors.title?.message}</p>
                 </div>
                 <div className="form-block">
                     <label>Зарплата шек/час</label>
-                    <input {...register("salary")} defaultValue="33" />
+                    <input type="number" {...register("salary")} defaultValue="33" />
+                    <p className="errorInput">{errors.salary?.message}</p>
+                    <span>Это значение будет учитываться при расчете рабочих часов</span>
                 </div>
                 <div className="form-block">
                     <label>Перерыв на смене (в минутах)</label>
-                    <input {...register("break")} placeholder="30" defaultValue="30"/>
+                    <input type="number" {...register("break")} placeholder="30" defaultValue="30"/>
+                    <p className="errorInput">{errors.break?.message}</p>
                     <span>Это значение будет учитываться при расчете рабочих часов</span>
                 </div>
                 <div className="form-block">
--- src/routes/work-worker-new.jsx
+++ src/routes/work-worker-new.jsx
@@ -5,12 +5,22 @@ import {addDoc, collection, getFirestore} from "firebase/firestore";
 import {useContext, useEffect} from "react";
 import UserContext from "../context/UserContext";
 import style from "../components/event-new/EventNew.module.css";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object({
+    title: yup.string().max(255, 'Название должно быть не более 255 символов').required('Название обязательное поле'),
+    salary: yup.number().typeError('Зарплата должна быть числом').max(99, 'Зарплата должна быть не более 2 символов').positive('Зарплата должна быть положительным числом').integer().required('Зарплата обязательное поле'),
+    break: yup.number().typeError('Перерыв должен быть числом').max(99, 'Перерыв должен быть не более 3 символов').moreThan(-1, 'Перерыв должен быть положительным числом').integer(),
+}).required();
 
 export default function WorkWorkerNew() {
     const user = useContext(UserContext);
     const navigate = useNavigate();
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
     useEffect(() => {
         if (user.role !== 'worker') {
@@ -38,14 +48,18 @@ export default function WorkWorkerNew() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-block">
                     <input {...register("title")} placeholder="Название" />
+                    <p className="errorInput">{errors.title?.message}</p>
                 </div>
                 <div className="form-block">
                     <label>Зарплата шек/час</label>
-                    <input {...register("salary")} defaultValue="33" />
+                    <input type="number" {...register("salary")} defaultValue="33" />
+                    <p className="errorInput">{errors.salary?.message}</p>
+                    <span>Это значение будет учитываться при расчете рабочих часов</span>
                 </div>
                 <div className="form-block">
                     <label>Перерыв на смене (в минутах)</label>
-                    <input {...register("break")} placeholder="30" defaultValue="30"/>
+                    <input type="number" {...register("break")} placeholder="30" defaultValue="30"/>
+                    <p className="errorInput">{errors.break?.message}</p>
                     <span>Это значение будет учитываться при расчете рабочих часов</span>
                 </div>
                 <div className="form-block">
